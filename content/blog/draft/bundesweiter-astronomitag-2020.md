17142159f46831a88ca79793f0ddee574dde6b15
Alexander Peerdeman <a@peerdeman.info>
2020-11-08 19:57:29 +0100

    Begin implementing migration script for VHS

---
+++ content/blog/draft/bundesweiter-astronomitag-2020.md
@@ -0,0 +1,58 @@
+---
+title: Bundesweiter Astronomietag 2020
+date: 2020-03-07 11:13:05
+images: 
+summary: TODO...
+draft: true
+---
+Wir laden ein zum:
+
+# __Astronomietag 2020  
+__
+
+&nbsp;
+
+Am Samstag, den 28. März 2020 sind wir ab 13:00 Uhr für Sie da.
+
+---
+
+### __<span style="text-decoration: underline;">Der Eintritt ist frei!</span>__
+
+---
+
+Am Nachmittag haben Sie die Gelegenheit mit uns auf der Sonne die gigantischen Eruptionen anzuschauen.
+
+  
+Es gibt interessante Vorträge:
+
+Das Sonnensystem  
+ Die Sonne  
+Der Mond  
+Die Plejaden  
+Astrofotografie  
+Die coole Freeware Stellarium  
+ und weiteren Themen.
+
+---
+
+Bei gutem Wetter starten wir wieder unsere Modellraketen.
+
+---
+
+Fürs Catering sorgt unsere „Redlight Bar“
+
+---
+
+Wenn es dunkel ist, schauen wir uns mit unseren Instrumeten, funkelnde Sterne, großartige Nebel und die Überreste von Sternexplosionen und viele andere tolle Objekte am Himmel an.
+
+---
+
+Bitte denken Sie an entsprechende __warme Kleidung!!__
+
+&nbsp;
+
+Wir freuen wir uns auf Sie/Euch, - lasst uns zusammen ein tolles Astroevent feiern!
+
+&nbsp;
+
+Ihr Team von der Sternwarte Bad Kreuznach e.V.
--- migration/format.py
+++ migration/format.py
@@ -1,29 +1,69 @@
 import json
 import html2markdown
+import requests
+import shutil
 
 if __name__ == "__main__":
 
-    with open("SiteTree.json", "r") as json_file:
+    with open("db1033256-1.json", "r") as json_file:
         data = json.load(json_file)
-        posts = data[2]['data']
 
-        print(posts[0])
 
-        for post in posts:
+        ## List all tables
+        # for idx, entry in enumerate(data[2:]):
+        #     print(idx, entry["name"])
 
-            plain = html2markdown.convert(post["Content"])
-            filename = post["URLSegment"] + ".md"
-            print(filename)
+        ## List all field of vortrag
+        for x in data[88]["data"]:
+            print(x.keys())
+            print(x["BildID"])
+            break
 
-            out = "---\n"
-            out += "title: {}\n".format(post["Title"])
-            out += "date: {}\n".format(post["Created"])
-            out += "images: \n"
-            out += "summary: TODO...\n"
-            out += "---\n"
-            out += plain
+            # Content
+            # Created
+            # Datum
+            # Referent
+            # Title
+            # UhrzeitAnfang
+            # BildID
+            # Preis (+ Ermäßigt)
+            # IsZusatz
+            # Keine Details?
 
-            print(out)
 
-            with open("../content/blog/" + filename, "w") as target_file:
-                target_file.write(out)
+
+        file_database_id = 43
+        image_id = 278
+
+        image_object = data[file_database_id]["data"][image_id]
+
+        print(data[43]["data"][278].keys())
+        print(image_object)
+
+        image_file_name = image_object["Name"]
+        image_file_title = image_object["Title"]
+        image_file_location = image_object["Filename"]
+
+        print("### File: {}\nTitle: {}\n{}\n".format(image_file_name, image_file_title, image_file_location))
+
+        download_url = "https://sternwarte-kreuznach.de/" + image_file_location
+
+        print("Attemting to download from {}".format(download_url))
+        r = requests.get(download_url, stream=True)
+        print(r)
+
+        if r.status_code == 200:
+            # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+            r.raw.decode_content = True
+
+            # Open a local file with wb ( write binary ) permission.
+            with open(image_file_name, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+
+            print('Image sucessfully Downloaded: ', image_file_name)
+        else:
+            print('Image could not be retreived!')
+
+
+
+
